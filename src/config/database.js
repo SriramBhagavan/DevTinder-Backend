982efabc93dd8bc72639757b6a8ab532f0267b0c
SriramBhagavan <srirambhagavan441@gmail.com>
2025-09-27 16:04:39 +0530

    added env file

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@
 const mongoose=require('mongoose')
 
 const connectDB=async ()=>{
-    await mongoose.connect("mongodb+srv://Bhagavan:Mongodb%40545@cluster0.gnjempb.mongodb.net/devTinder");
+    await mongoose.connect(process.env.DB_CONNECTION_SECRET);
 
 
 }
