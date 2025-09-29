4adae80e9af6447e5bfb0a87a9451b905d510b31
SriramBhagavan <srirambhagavan441@gmail.com>
2025-09-29 16:39:23 +0530

    added websocket configuration

--- src/app.js
+++ src/app.js
@@ -3,8 +3,10 @@ const connectDB = require('./config/database');
 const cookieParser = require('cookie-parser');
 const cors = require("cors");
 require("dotenv").config();
+const http=require('http')
 
 const app = express();
+
 
 
 app.use(cors({
@@ -24,6 +26,7 @@ const profileRouter = require('./routes/profile');
 const requestRouter = require('./routes/requests');
 const userRouter = require('./routes/user');
 const paymentRouter = require('./routes/payment');
+const initializeSocket = require('./utils/socket');
 
 
 app.use('/', authRouter);
@@ -32,9 +35,14 @@ app.use('/', requestRouter);
 app.use('/', userRouter);
 app.use('/',paymentRouter)
 
+const server=http.createServer(app)
+initializeSocket(server)
+
+
+
 connectDB()
   .then(() => {
       console.log("Database connection established");
-      app.listen(3000, () => console.log('server listening on port 3000'));
+      server.listen(3000, () => console.log('server listening on port 3000'));
   })
   .catch((err) => console.error("database cannot be connected", err));
